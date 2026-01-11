be02cbe8764523a31a5aef07ecde16d73f5e59f4
HanDcLaP-Lab <2678493078@qq.com>
2026-01-11 16:12:06 +0800

    first commit

---
+++ project/code/wireless_uart.h
@@ -0,0 +1,12 @@
+#ifndef _WIRELESS_UART_H
+#define _WIRELESS_UART_H
+
+#include "zf_common_headfile.h"
+#include <math.h>
+#include <stdint.h>
+
+void wireless_uart_init_();
+void wireless_uart_get_();
+void wireless_uart_send_int(int32_t send_a);
+void wireless_uart_send_float(float send_a);
+#endif
