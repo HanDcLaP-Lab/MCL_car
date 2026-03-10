e7847b12d0fa3ebd8a0bdc0a63fd275e6142caab
HanDcLaP-Lab <2678493078@qq.com>
2026-03-10 22:02:32 +0800

    3.9d

--- project/code/test.c
+++ project/code/test.c
@@ -22,7 +22,7 @@ void test_program_1(void)
     Mecanum_Unlock();
     // Mecanum_Set_Velocity(1.0f, 0.0f, 0.0f);
     // system_delay_ms(3000);
-    Mecanum_Set_Velocity(0.0f, 0.7f, 0.0f);
+    Mecanum_Set_Velocity(0.0f, 0.4f, 0.0f);
     system_delay_ms(3000);
     Mecanum_Set_Velocity(0.0f, 0.0f, 0.0f);
     system_delay_ms(3000);
--- project/user/main_cm4.c
+++ project/user/main_cm4.c
@@ -66,7 +66,12 @@ int main(void)
     
     Mecanum_Set_Velocity(0.0f, 0.0f, 0.0f);
     pit_ms_init(PIT_CH0, 1);
-    system_delay_ms(3000);
+    while (imu_car_data.is_calibrated == 0) {
+        Parse_Board_Uart_Data();
+        seekfree_assistant_data_analysis();
+        system_delay_ms(1); 
+    }
+    Mecanum_Unlock();
 
     test_program_1();
     
@@ -78,7 +83,7 @@ int main(void)
 
         if (board_rx_complete_flag) {
             board_rx_complete_flag = 0;
-            Visual_Control_Loop();
+            //Visual_Control_Loop();
         }
 
         seekfree_assistant_data_analysis();
