24047602543c184c3e981ddcd0b183492df4fd85
HanDcLaP-Lab <2678493078@qq.com>
2026-03-16 17:29:19 +0800

    3.16

--- project/code/mecnum.h
+++ project/code/mecnum.h
@@ -32,9 +32,12 @@
 #define MOTOR_RB_DIR    P06_3               // 右后 DIR
 
 //记忆功能
-#define COAST_TIME_MS  0 // 目标丢失后的记忆滑行时间 (毫秒)
+#define COAST_CNT 3 // 目标丢失后的记忆次数(毫秒)
 #define COAST_DECAY   0.998f // 速度衰减系数 (每次循环衰减)
 #define EDGE_DISTANCE 80.0 //边界判定距离
+
+#define EDGE_X 50.0f
+#define EDGE_Y 70.0f
 
 extern int EN;
 // ================== 结构体定义 ==================
