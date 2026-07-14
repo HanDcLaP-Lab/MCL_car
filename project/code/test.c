24dc2323922baeac207d39e4605131c11a70a1bd
KlemdRick_hy <3304924652@qq.com>
2026-07-15 00:15:04 +0800

    7.14d

--- project/code/test.c
+++ project/code/test.c
@@ -23,13 +23,13 @@ void test_program_1(void)
 
     while(1){
         Mecanum_Set_Velocity(0.75f, 0.0f, 0.0f);
-        system_delay_ms(3500);
+        system_delay_ms(2500);
         Mecanum_Set_Velocity(0.0f, 0.0f, 0.0f);
-        system_delay_ms(2000);
+        system_delay_ms(500);
         Mecanum_Set_Velocity(-0.75f, 0.0f, 0.0f);
-        system_delay_ms(3500);
+        system_delay_ms(2500);
         Mecanum_Set_Velocity(0.0f, 0.0f, 0.0f);
-        system_delay_ms(2000);
+        system_delay_ms(3000);
     }
 
     Chassis_Block(DISARM_MANUAL);
