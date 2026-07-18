343054600f8d1bb06c4b02b7ac629371a6ad3967
KlemdRick_hy <3304924652@qq.com>
2026-07-18 10:58:04 +0800

    453636

--- project/code/config.h
+++ project/code/config.h
@@ -5,7 +5,7 @@
 
 #if STRATEGY == 1
 // ================== 小车运动参数 ==================
-#define TARGET_SPEED            0.8f   // 目标速度 (m/s)
+#define TARGET_SPEED            0.7f   // 目标速度 (m/s)
 #define MAX_ACCEL_LINEAR        5.0f   // 平移加速度上限 (m/s^2)
 #define LARGE_TURN_ACCEL_SCALE  0.3f   // 大角度换向时的平移加速度倍率
 #define MAX_ACCEL_W             1.0f   // 旋转加速度上限 (rad/s^2)
@@ -35,7 +35,7 @@
 #define MAX_ACCEL_W             1.0f   // 旋转加速度上限 (rad/s^2)
 
 // ================== Dash 参数 ==================
-#define DASH_DURATION_COMPENSATION_MS 200  // 计算时长限幅后的有符号补偿，可正可负 (ms)
+#define DASH_DURATION_COMPENSATION_MS 400  // 计算时长限幅后的有符号补偿，可正可负 (ms)
 #define POST_DASH_HOLD_MS             0U   // 完全刹停后静止等待视觉稳定 (ms)
 #endif
 
